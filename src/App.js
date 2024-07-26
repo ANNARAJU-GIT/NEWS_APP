65bf9892a6abfc1d1326d0eb6db9f1bae25e28aa
Annaraju <annaraju311@gmail.com>
2024-07-27 02:24:02 +0530

    Newspaper subscription app created

--- src/App.js
+++ src/App.js
@@ -4,12 +4,18 @@ import AddSubscribers from './components/AddSubscribers';
 import ViewSubscribers from './components/ViewSubscribers';
 import Search from './components/Search';
 import Delete from './components/Delete';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-      <Delete/>
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<AddSubscribers/>}/>
+      <Route path='/search' element={<Search/>}/>
+      <Route path='/delete' element={<Delete/>}/>
+      <Route path='/view' element={<ViewSubscribers/>}/>
+    </Routes>
+    </BrowserRouter>
   );
 }
 
