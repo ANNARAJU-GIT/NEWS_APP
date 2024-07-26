bc425efcee9bb6907371d15923a1bd687fefe218
Annaraju <annaraju311@gmail.com>
2024-07-27 01:51:00 +0530

    Search Subscribers created

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 import AddSubscribers from './components/AddSubscribers';
 import ViewSubscribers from './components/ViewSubscribers';
+import Search from './components/Search';
 
 function App() {
   return (
     <div>
-      <ViewSubscribers/>
+      <Search/>
     </div>
   );
 }
